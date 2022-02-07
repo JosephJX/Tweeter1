a656227761df04f7bc558abe833de15e367a3550
Joseph McGregor <joseph00@my.yorku.ca>
2022-02-07 07:00:14 -0500

    form submission using jquery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,38 +7,42 @@
 $(document).ready(function () {
 
 
+  $('.new-tweet-form').on('submit', function (event) {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
+    event.preventDefault();
+    console.log('Submit is being triggered')
+
+    const data = [
+      {
+        "user": {
+          "name": "Newton",
+          "avatars": "https://i.imgur.com/73hZDYK.png"
+          ,
+          "handle": "@SirIsaac"
+        },
+        "content": {
+          "text": "If I have seen further it is by standing on the shoulders of giants"
+        },
+        "created_at": 1461116232227
       },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+      {
+        "user": {
+          "name": "Descartes",
+          "avatars": "https://i.imgur.com/nlhLi3I.png",
+          "handle": "@rd"
+        },
+        "content": {
+          "text": "Je pense , donc je suis"
+        },
+        "created_at": 1461113959088
+      }
+    ];
 
 
-  const createTweetElement = function (tweet) {
-    let date = timeago.format(tweet.created_at);
-    let $tweet = $(
-      `<article class="tweet-header wrapper">
+    const createTweetElement = function (tweet) {
+      let date = timeago.format(tweet.created_at);
+      let $tweet = $(
+        `<article class="tweet-header wrapper">
          <header>
                <img class="left" id="pic" src="${tweet.user.avatars}">
                <h1 class="left">${tweet.user.name}</h1>
@@ -56,19 +60,26 @@ $(document).ready(function () {
            </div>
          </footer>
          </article> `)
-    console.log("testing", $tweet);
-    return $tweet;
-  };
+      console.log("testing", $tweet);
+
+      return $tweet;
+    };
 
 
-  const renderTweets = function (tweets) {
-    $("#tweets").empty();
-    console.log($('.tweets-container'));
-    for (let tweet of tweets) {
-      $('.tweets-container').prepend(createTweetElement(tweet));
-      console.log(tweets);
+    const renderTweets = function (tweets) {
+      $("#tweets").empty();
+      console.log($('.tweets-container'));
+      for (let tweet of tweets) {
+        $('.tweets-container').prepend(createTweetElement(tweet));
+        console.log(tweets);
+      }
     }
-  }
-  renderTweets(data);
+    renderTweets(data);
+
+    // const $newTweet = $('#submit-tweet');
+    // $newTweet.on('submit', function (event) {
+    //   event.preventDefault();
+    // });
+  })
 
 });  
