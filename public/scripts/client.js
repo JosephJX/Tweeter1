4152098d7fc31066497afdc62166e81086d7ff7e
Joseph McGregor <joseph00@my.yorku.ca>
2022-02-07 07:10:15 -0500

    getting tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,37 +6,52 @@
 
 $(document).ready(function () {
 
+  const loadTweets = function () {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+
+    }).then((response) => {
+      // console.log("RESPONSE: ");
+      // console.log(response);
+      renderTweets(response);
+      $('#tweet-text').val("");
+      $(".counter").text(140);
+    });
+  };
+  loadTweets();
+
 
   $('.new-tweet-form').on('submit', function (event) {
 
     event.preventDefault();
     console.log('Submit is being triggered')
 
-    const data = [
-      {
-        "user": {
-          "name": "Newton",
-          "avatars": "https://i.imgur.com/73hZDYK.png"
-          ,
-          "handle": "@SirIsaac"
-        },
-        "content": {
-          "text": "If I have seen further it is by standing on the shoulders of giants"
-        },
-        "created_at": 1461116232227
-      },
-      {
-        "user": {
-          "name": "Descartes",
-          "avatars": "https://i.imgur.com/nlhLi3I.png",
-          "handle": "@rd"
-        },
-        "content": {
-          "text": "Je pense , donc je suis"
-        },
-        "created_at": 1461113959088
-      }
-    ];
+    // const data = [
+    //   {
+    //     "user": {
+    //       "name": "Newton",
+    //       "avatars": "https://i.imgur.com/73hZDYK.png"
+    //       ,
+    //       "handle": "@SirIsaac"
+    //     },
+    //     "content": {
+    //       "text": "If I have seen further it is by standing on the shoulders of giants"
+    //     },
+    //     "created_at": 1461116232227
+    //   },
+    //   {
+    //     "user": {
+    //       "name": "Descartes",
+    //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+    //       "handle": "@rd"
+    //     },
+    //     "content": {
+    //       "text": "Je pense , donc je suis"
+    //     },
+    //     "created_at": 1461113959088
+    //   }
+    // ];
 
 
     const createTweetElement = function (tweet) {
@@ -60,7 +75,6 @@ $(document).ready(function () {
            </div>
          </footer>
          </article> `)
-      console.log("testing", $tweet);
 
       return $tweet;
     };
@@ -71,7 +85,6 @@ $(document).ready(function () {
       console.log($('.tweets-container'));
       for (let tweet of tweets) {
         $('.tweets-container').prepend(createTweetElement(tweet));
-        console.log(tweets);
       }
     }
     renderTweets(data);
