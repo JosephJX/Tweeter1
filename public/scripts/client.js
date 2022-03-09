c0435f440e26e064b00a741ca5da232250fcc0dc
Joseph McGregor <joseph00@my.yorku.ca>
2022-03-09 18:43:54 -0500

    removed mock data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -25,31 +25,6 @@ $(document).ready(function () {
     return div.innerHTML;
   };
 
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd"
-  //     },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ];
   const createTweetElement = function (tweet) {
     let date = timeago.format(tweet.created_at);
     let $tweet = $(
@@ -83,6 +58,19 @@ $(document).ready(function () {
       $('.tweets-container').prepend(createTweetElement(tweet));
     }
   }
+  const errorBanner = function (err) {
+    if (!$('.invalid-tweet').hasClass("toggled-invalid-tweet")) {
+      $('.invalid-tweet').addClass('toggled-invalid-tweet');
+    }
+    $('.invalid-tweet')
+      .text(`Error: ${err}`)
+      .slideDown(250, function () {
+        setTimeout(function () {
+          $('.toggled-invalid-tweet').slideUp(250);
+        }, 3500);
+      });
+  };
+
   // const $newTweet = $('#submit-tweet');
   // $newTweet.on('submit', function (event) {
   //   event.preventDefault();
