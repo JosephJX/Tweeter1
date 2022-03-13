b018ba0280f48fe0357ea63c25c4df196598723a
Joseph McGregor <joseph00@my.yorku.ca>
2022-03-13 15:40:45 -0400

    fix order of tweets

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,10 @@
-const tweetText = document.getElementById("tweet-text");
-const counter = document.getElementsByName("counter");
-
-
-$(tweetText).on("input", function () {
+$("#tweetText").on("input", function () {
   const count = 140;// $(counter).val
   const tweetCount = $(this).val().length;
-  $(counter).text(count - tweetCount);
-  if ($(counter).val() < 0) {
-    $(counter).css("color", "red")
+  $(".counter").text(count - tweetCount);
+  if ($(".counter").val() < 0) {
+    $(".counter").css("color", "red")
   } else {
-    $(counter).css("color", "black")
+    $(".counter").css("color", "black")
   }
 })
