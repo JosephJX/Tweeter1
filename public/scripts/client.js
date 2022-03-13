b2617a7dcc25f3933001b78295ef93536140299b
Joseph McGregor <joseph00@my.yorku.ca>
2022-03-13 15:29:14 -0400

    adds dynamic tweets that dont need hard coding

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,105 +1,70 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 $(document).ready(function () {
 
-  const loadTweets = function () {
-    $.ajax({
-      method: 'GET',
-      url: '/tweets',
-
-    }).then((response) => {
-      renderTweets(response);
-      $('#tweet-text').val("");
-      $(".counter").text(140);
-    });
-  };
-  loadTweets();
-
-  const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
-
-  const createTweetElement = function (tweet) {
-    let date = timeago.format(tweet.created_at);
-    let $tweet = $(
-      `<article class="tweet-header wrapper">
-         <header>
-               <img class="left" id="pic" src="${tweet.user.avatars}">
-               <h1 class="left">${tweet.user.name}</h1>
-             <h5 class="right">${tweet.user.handle}</h5>
-         </header>
-         <p class="message">${tweet.content.text}</p
-         <footer class="tweet-footer">
-           <div>
-           <p class="left"> ${date}</p>
-           </div>
-           <div>
-             <i class="fas fa-retweet"></i>
-             <i class="fas fa-heart"></i>
-             <i class="fas fa-flag"></i>
-           </div>
-         </footer>
-         </article> `)
-
-    return $tweet;
-  };
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
 
-  const renderTweets = function (tweets) {
-    $("#tweets").empty();
-    console.log($('.tweets-container'));
+
+  const renderTweets = (tweets) => {
+    // loops through tweets
     for (let tweet of tweets) {
-      $('.tweets-container').prepend(createTweetElement(tweet));
+      // calls createTweetElement for each tweet
+      const $tweet = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      console.log($tweet);
+      $("#tweets-container").append($tweet);
+      // console.log($("#tweets-container"));
     }
-  }
-  const errorBanner = function (err) {
-    if (!$('.invalid-tweet').hasClass("toggled-invalid-tweet")) {
-      $('.invalid-tweet').addClass('toggled-invalid-tweet');
-    }
-    $('.invalid-tweet')
-      .text(`Error: ${err}`)
-      .slideDown(250, function () {
-        setTimeout(function () {
-          $('.toggled-invalid-tweet').slideUp(250);
-        }, 3500);
-      });
   };
 
-  // const $newTweet = $('#submit-tweet');
-  // $newTweet.on('submit', function (event) {
-  //   event.preventDefault();
-  // });
-  const $newTweet = $('#tweet-form');
-  $newTweet.on('submit', function (event) {
-    event.preventDefault();
-    const tweet = $("#tweet-text").val().length;
-    console.log(tweet);
-    if (!tweet) {
-      $('#errorMessage').show();
-      $('#errorMessage').text("Tweet cannot be empty!");
-    };
-    if (tweet > 140) {
-      $('#errorMessage').show();
-      $('#errorMessage').text("Tweet can't be longer than 140 characters!");
-    } else {
-      const val = $(this).serialize();
-      $.ajax("/tweets", {
-        method: "POST",
-        data: val,
-      })
-        .then(() => {
-          $('#errorMessage').hide();
-          loadTweets();
-          $("#tweet-text").val("");
+  const createTweetElement = (tweet) => {
+    let $tweet = $("<article>").addClass("tweet");
+    let $html = `
+    <header>
+      <span class="user">
+        <img class="userAvatar" src=${tweet.user.avatars}>
+        <p class="username">${tweet.user.name}</p>
+      </span>
+      <div class="handle">${tweet.user.handle}</div>
+    </header>
+    <p>${tweet.content.text}</p>
+    <footer>
+      <span>${timeago.format(tweet.created_at, 'pt_BR')}</span>
+      <span>
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </span>
+    </footer>
+    `;
+    $tweet.append($html)
+    return $tweet;
 
-        });
-    }
-  })
+  };
 
-});  
+  renderTweets(data);
+
+});
