7c6da25950bb94a8b4e6d67fc8c97429bbae1d77
Joseph McGregor <joseph00@my.yorku.ca>
2022-03-13 19:45:43 -0400

    refactor

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,12 @@
-$("#tweet-form-text").on("input", function () {
-  const count = 140;// $(tweet-form-counter).val
-  const tweetCount = $(this).val().length;
-  $(".tweet-form-counter").text(count - tweetCount);
-  if (tweetCount > 140) {
-    $(".tweet-form-counter").css("color", "red");
-  } else {
-    $(".tweet-form-counter").css("color", "black");
-  }
+$(document).ready(function () {
+  $("#tweet-form-text").on("input", function () {
+    const count = 140;// $(tweet-form-counter).val
+    const tweetCount = $(this).val().length;
+    $(".tweet-form-counter").text(count - tweetCount);
+    if (tweetCount > 140) {
+      $(".tweet-form-counter").css("color", "red");
+    } else {
+      $(".tweet-form-counter").css("color", "black");
+    }
+  })
 })
