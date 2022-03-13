c0e288dc95ab847f015dbb9ca39688b67bd82c72
Joseph McGregor <joseph00@my.yorku.ca>
2022-03-13 19:45:57 -0400

    nothing

---
+++ public/scripts/icon-color-change.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  const retweet = document.getElementsByClassName("i.fas.fa-retweet");
+
+  $(retweet).on("mouseover", function () {
+    if (M) {
+      $(retweet).css("color", "yellow")
+    } else {
+      $(retweet).css("color", "#2980b9")
+    }
+  })
+})
